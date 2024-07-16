43f6a3a5f839162a9e64a3c042dc7a5077c7a796
Debiddo <dzheng@terpmail.umd.edu>
2024-07-15 21:59:18 -0400

    create an image slide show

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import logoImg from '@/assets/logo.png'
 import styles from './main-header.module.css'
@@ -7,7 +8,7 @@ const MainHeader = () => {
     return (
         <header className={styles.header}>
             <Link className={styles.logo} href='/'>
-                <img src={logoImg.src} alt="a plate with food on it" />
+                <Image src={logoImg} alt="a plate with food on it" priority />
                 NextLevel Food
             </Link>
             <nav className={styles.nav}>
