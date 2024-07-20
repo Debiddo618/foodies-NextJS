7d9c22b79b2deed7aa8d829b23e2ad4e5a148589
Debiddo <dzheng@terpmail.umd.edu>
2024-07-20 13:01:03 -0400

    created a meal not found and meal detail page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,7 +1,38 @@
-const MealDetails = () => {
+import { getMeal } from '@/lib/meals';
+import styles from './page.module.css';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+const MealDetails = ({params}) => {
+    const meal = getMeal(params.slug);
+
+    if(!meal){
+        // if meal is not found, then navigate to the closes not found page
+        notFound();
+    }
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />')
     return ( 
         <>
-            <h1>Meal Details</h1>
+            <header className={styles.header}>
+                <div className={styles.image}>
+                    <Image src={meal.image} fill/>
+                </div>
+                <div className={styles.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={styles.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={styles.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p className={styles.instructions} dangerouslySetInnerHTML={{
+                    __html:meal.instructions,
+                }}>
+
+                </p>
+            </main>
         </>
     );
 }
